8ba68a478cc14e5c97e1eefc3de7433cf8d63d72
pak <lazer.bumblebee@gmail.com>
2024-07-28 21:25:06 +1000

    initial

---
+++ gen_replay.sh
@@ -0,0 +1,20 @@
+cat vidbot_unhighlight.json | jq '.steps' > tmp_steps.json
+
+for i in {1..45}
+do
+jqArg="$jqArg tmp_steps.json"
+done
+
+cat ./vidbot_init_config.json | jq '.steps' | jq -s add - $jqArg > tmp.json
+
+cat << EOF > output.json
+{
+  "title": "remove highlights",
+  "steps":
+EOF
+
+cat tmp.json >> output.json
+
+echo "}" >> output.json
+
+rm -rf tmp.json tmp_steps.json
